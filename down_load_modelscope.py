206f1f37c71680e0a020ae4465c81a5b0a39eefd
Chia-Chun Wu <ccwu0918@gmail.com>
2024-04-04 17:47:07 +0800

    Update down_load_modelscope.py

--- down_load_modelscope.py
+++ down_load_modelscope.py
@@ -1,5 +1,9 @@
 from modelscope.hub.snapshot_download import snapshot_download
 
+# model_dir = snapshot_download('Xunzillm4cc/Xunzi-Qwen-Chat', 
+#                               cache_dir='./Xunzi-Qwen-Chat', 
+#                               revision='master')
+
 model_dir = snapshot_download('Xunzillm4cc/Xunzi-Qwen-Chat', 
-                              cache_dir='./Xunzi-Qwen-Chat', 
+                              cache_dir='../', 
                               revision='master')
